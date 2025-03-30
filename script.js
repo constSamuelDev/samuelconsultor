c0c1fac940801fd81e8bd908f63a2c6914ab9365
constSamuelDev <sammuelvieiraa@gmail.com>
2025-03-29 23:01:07 -0300

    mudança envio formulario

--- script.js
+++ script.js
@@ -9,13 +9,36 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
+    // Inicialização do EmailJS
+    emailjs.init("g1c7VypWC9g4u7QPl");
+
     // Tratamento do formulário de contato
     const formularioContato = document.getElementById('formulario-contato');
     if (formularioContato) {
         formularioContato.addEventListener('submit', function(e) {
             e.preventDefault();
-            alert('Obrigado pelo contato! Retornaremos em breve.');
-            formularioContato.reset();
+
+            const formData = {
+                to_name: "Samuel",
+                from_name: this.nome.value,
+                message: `
+                    Nome: ${this.nome.value}
+                    Renda: R$ ${this.renda.value}
+                    Tipo de Renda: ${this.tipo_renda.value}
+                    Dependentes: ${this.dependentes.value}
+                    Carteira Assinada +3 anos: ${this.carteira_assinada.value}
+                    Contato: ${this.contato.value}
+                `
+            };
+
+            emailjs.send('service_z2zcv1n', 'template_4xe9kbz', formData)
+                .then(function() {
+                    alert('Mensagem enviada com sucesso!');
+                    formularioContato.reset();
+                }, function(error) {
+                    alert('Erro ao enviar mensagem. Por favor, tente novamente.');
+                    console.error('Erro:', error);
+                });
         });
     }
 
